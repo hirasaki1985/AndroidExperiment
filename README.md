b8ec5326a19d5efcd465200f19852c894af50e3c
hirasaki1985 <hirasaki1985@gmail.com>
2019-02-19 12:52:04 +0900

    set bottom navigation bar

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## icon list
 https://material.io/tools/icons/?style=baseline
+
+
+## design like ios
+https://qiita.com/ogapants/items/8a551ed3b16bcf0cc3ed
--- app/src/main/java/com/example/hirasaki/androidexperiment/MainActivity.kt
+++ app/src/main/java/com/example/hirasaki/androidexperiment/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.hirasaki.androidexperiment
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_timeline -> {
+                // message.setText(R.string.nav_title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_friends -> {
+                // message.setText(R.string.nav_title_friends)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                // message.setText(R.string.nav_title_profile)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
